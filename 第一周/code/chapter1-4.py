f1b39bfba41215b45e4c818ff52a9e7ba7af8bc2
njustcodingjs <jszhao@njust.edu.cn>
2019-03-14 03:28:53 +0800

    Create chapter1-4.py

---
+++ 第一周/code/chapter1-4.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 03:05:56 2019
+
+@author: js
+"""
+surname = input("please input your surname： ")
+firstname = input("please input your firstname： ")
+print("Your name is",surname+' '+firstname)
